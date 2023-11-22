8478f3e0b15bb65ef82047b9cddaba0387a3dd42
Buenza123 <ubledbuen@gmail.com>
2023-11-22 21:43:19 +0700

    ads

--- enemy/ads_file/ads_file.gd
+++ enemy/ads_file/ads_file.gd
@@ -3,9 +3,9 @@ extends CharacterBody2D
 @onready var target = %folder
 var draggingDistance
 var dir
-var dragging_folder
+var dragging_ads
 var newPosition = Vector2()
-var closing_folder
+var closing_ads
 @export var speed_x = 7
 @export var speed_y = 7
 @export var speed = 200
@@ -24,23 +24,23 @@ func _input(event):
 		if event.is_pressed() && mouse_in:
 			draggingDistance = position.distance_to(get_viewport().get_mouse_position())
 			dir = (get_viewport().get_mouse_position() - position).normalized()
-			dragging_folder = true
+			dragging_ads = true
 			newPosition = get_viewport().get_mouse_position() - draggingDistance * dir
 		else:
-			dragging_folder = false
+			dragging_ads = false
 			
 	elif event is InputEventMouseMotion:
-		if dragging_folder:
+		if dragging_ads:
 			newPosition = get_viewport().get_mouse_position() - draggingDistance * dir
 			
 			
-	if event is InputEventMouseButton and closing_folder == true:
+	if event is InputEventMouseButton and closing_ads == true:
 		if event.is_double_click() && mouse_in:
 			get_parent().remove_child(self)
 			self.queue_free()
 
 func _physics_process(delta):
-	if dragging_folder:
+	if dragging_ads:
 		velocity = (newPosition - position) * Vector2(speed_x, speed_y)
 		move_and_slide()
 
@@ -54,9 +54,9 @@ func _on_drag_area_mouse_exited():
 
 func _on_close_area_mouse_entered():
 	mouse_in = true
-	closing_folder = true
+	closing_ads = true
 
 
 func _on_close_area_mouse_exited():
 	mouse_in = false
-	closing_folder = true
+	closing_ads = true
