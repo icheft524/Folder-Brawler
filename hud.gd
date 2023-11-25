fa6aadeddf75068b69ae3e1aa5cd98f054c4c772
Buenza123 <ubledbuen@gmail.com>
2023-11-26 01:19:31 +0700

    asd

--- hud.gd
+++ hud.gd
@@ -11,11 +11,11 @@ func _process(delta):
 	$TextureRect/time.text = "Timer: " + str(global.time)
 	
 	$TextureRect/wave.text = "Wave: " + str(1)
-	if global.time == 21: #wave2
+	if global.time >= 21: #wave2
 		$TextureRect/wave.text = "Wave: " + str(2)
-	elif global.time == 41: #wave3
+	elif global.time >= 41: #wave3
 		$TextureRect/wave.text = "Wave: " + str(3)
-	elif global.time == 61: #wave4
+	elif global.time >= 61: #wave4
 		$TextureRect/wave.text = "Wave: " + str(4)
-	elif global.time == 81: #wave5
+	elif global.time >= 81: #wave5
 		$TextureRect/wave.text = "Wave: " + str(5)
