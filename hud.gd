fee6dff0d30115e680e31cdc51d60afb7174551c
GuyShinto <xiiioffice000@gmail.com>
2023-11-27 13:08:55 +0700

    timer mins:secs
    1:08 PM 11/27

--- hud.gd
+++ hud.gd
@@ -2,15 +2,20 @@ extends Control
 
 @onready var player = global.target
 
+var secs = 0
+var mins = 0
+
 func _ready():
 	pass # Replace with function body.
 
 
 
 func _process(delta):
+	secs = fmod(global.time,60)
+	mins = fmod(global.time,60*60) / 60
 	$VBoxContainer/capacity.value = player.capacity
 	$VBoxContainer/capacity.max_value = player.max_capacity
-	$VBoxContainer/time.text = "Timer: " + str(global.time)
+	$VBoxContainer/time.text = "Timer: %02d:%02d" % [mins,secs]
 	
 	$VBoxContainer/wave.text = "Wave: " + str(1)
 	if global.time >= 21: #wave2
