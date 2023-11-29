54aaf1c3e71a5a7b81c5924857b26334bd0db0e6
Buenza123 <ubledbuen@gmail.com>
2023-11-29 21:57:56 +0700

    pop up

--- popup.gd
+++ popup.gd
@@ -1,12 +1,11 @@
 extends Node2D
 
-@onready var normal = $normal
-@onready var ads = $ads
-@onready var big = $big
-@onready var zip = $zip
-@onready var enc = $enc
+@onready var pic_wave1 = $wave1
+@onready var pic_wave2 = $wave2
+@onready var pic_wave3 = $wave3
+@onready var pic_wave4 = $wave4
+@onready var close_but = $close
 
-var tutorial = true
 var mouse_close_in = false
 var already_clicked = false
 #@export var close =  Vector2(0.8,0.8)
@@ -14,13 +13,12 @@ var already_clicked = false
 var wave2 = true
 var wave3 = true
 var wave4 = true
-var wave5 = true
-var wave6 = true
+
 
 func _ready():
-	if tutorial == true:
-		get_tree().paused = true
-		normal.visible = true
+	get_tree().paused = true
+	pic_wave1.visible = true
+	close_but.visible = true
 		
 		
 
@@ -28,75 +26,37 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if global.time == 22 and wave2: #wave2 big
+		pic_wave2.visible = true
+		close_but.visible = true
 		get_tree().paused = true
-		big.visible = true
+
 	if global.time == 47 and wave3: #wave3 enc
+		pic_wave3.visible = true
+		close_but.visible = true
 		get_tree().paused = true
-		enc.visible = true
+
 	if global.time == 72 and wave4: #wave4 zip
+		pic_wave4.visible = true
 		get_tree().paused = true
-		zip.visible = true
+
+
+
+func _on_close_pressed():
+	pic_wave1.visible = false
+	close_but.visible = false
+	get_tree().paused = false
 	
-
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.is_action_pressed("leftclick") && mouse_close_in && !already_clicked && !global.inv_open:
-			already_clicked = true
-			get_viewport().set_input_as_handled()
-			#var tween_close = create_tween()
-			#tween_close.tween_property(self,"scale",close,0.2).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN)
-			#tween_close.play()
-
-
-func _on_normal_mouse_entered():
-	mouse_close_in = true
-
-
-func _on_normal_mouse_exited():
-	mouse_close_in = false
-	normal.visible = false
-	ads.visible = true
-
-
-
-func _on_ads_mouse_entered():
-	mouse_close_in = true
-
-
-func _on_ads_mouse_exited():
-	mouse_close_in = false
-	ads.visible = false
-	get_tree().paused = false
-
-
-func _on_big_mouse_entered():
-	mouse_close_in = true
-
-
-func _on_big_mouse_exited():
-	mouse_close_in = false
-	big.visible = false
-	wave2 = false
-	get_tree().paused = false
-
-
-func _on_enc_mouse_entered():
-	mouse_close_in = true
-
-
-func _on_enc_mouse_exited():
-	mouse_close_in = false
-	enc.visible = false
-	wave3 = false
-	get_tree().paused = false
-
-
-func _on_zip_mouse_entered():
-	mouse_close_in = true
-
-
-func _on_zip_mouse_exited():
-	mouse_close_in = false
-	zip.visible = false
-	wave4 = false
-	get_tree().paused = false
+	if global.time == 22 and wave2: #wave2 big
+		pic_wave2.visible = false
+		close_but.visible = false
+		get_tree().paused = false
+	
+	if global.time == 47 and wave2: #wave2 big
+		pic_wave3.visible = false
+		close_but.visible = false
+		get_tree().paused = false
+		
+	if global.time == 72 and wave2: #wave2 big
+		pic_wave4.visible = false
+		close_but.visible = false
+		get_tree().paused = false
