393da21ad3be1c738bef50a08c2c6edda1458986
GuyShinto <xiiioffice000@gmail.com>
2023-11-30 21:50:38 +0700

    pop test

--- popup.gd
+++ popup.gd
@@ -1,70 +1,59 @@
 extends Node2D
 
-@onready var pic_wave1 = $wave1
-@onready var pic_wave2 = $wave2
-@onready var pic_wave3 = $wave3
-@onready var pic_wave4 = $wave4
-@onready var close_but = $close
+@onready var image = $image
+@onready var close_but = $"image/close"
 
 var mouse_close_in = false
 var already_clicked = false
+var normal = Vector2(0.369,0.274)
 #@export var close =  Vector2(0.8,0.8)
 
 var wave2 = true
 var wave3 = true
 var wave4 = true
+var pop =  Vector2(0.05,0.05)
+var close =  Vector2(0.1,0.1)
 
 
 func _ready():
 	global.tutorial = true
 	get_tree().paused = true
-	pic_wave1.visible = true
+	image.visible = true
 	close_but.visible = true
+	showpo()
+	
 
+func showpo():
+	image.scale = normal
+	var tween = create_tween()
+	tween.tween_property(image,"scale",normal + pop,0.5).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	tween.play()
 
+func closepo():
+	global.tutorial = false
+	image.visible = false
+	close_but.visible = false
+	if !global.on_upgrade:
+		get_tree().paused = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if image.scale < Vector2(0.3,0.3):
+		closepo()
 	if global.time == 22 and wave2: #wave2 big
-		global.tutorial = true
-		pic_wave2.visible = true
-		close_but.visible = true
-		get_tree().paused = true
+		_ready()
+		wave2 = false
 
 	if global.time == 47 and wave3: #wave3 enc
-		global.tutorial = true
-		pic_wave3.visible = true
-		close_but.visible = true
-		get_tree().paused = true
+		_ready()
+		wave3 = false
 
 	if global.time == 72 and wave4: #wave4 zip
-		global.tutorial = true
-		pic_wave4.visible = true
-		close_but.visible = true
-		get_tree().paused = true
+		_ready()
+		wave4 = false
 
 
 
 func _on_close_pressed():
-	global.tutorial = false
-	pic_wave1.visible = false
-	close_but.visible = false
-	get_tree().paused = false
-	
-	if global.time == 22 and wave2: #wave2 
-		global.tutorial = false
-		pic_wave2.visible = false
-		close_but.visible = false
-		get_tree().paused = false
-	
-	if global.time == 47 and wave3: #wave3
-		global.tutorial = false
-		pic_wave3.visible = false
-		close_but.visible = false
-		get_tree().paused = false
-		
-	if global.time == 72 and wave4: #wave4
-		global.tutorial = false
-		pic_wave4.visible = false
-		close_but.visible = false
-		get_tree().paused = false
+	var tween_close = create_tween()
+	tween_close.tween_property(image,"scale",normal - close,0.4).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN)
+	tween_close.play()
