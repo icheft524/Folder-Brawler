bcec16ed1b877f53f536c95054bb09ac026b2c03
Nont Nonta <nontnta@gmail.com>
2023-12-02 03:45:07 +0700

    debug removed

--- global.gd
+++ global.gd
@@ -6,7 +6,7 @@ var time = 0
 var gameend = false
 var target
 var adstimelock = 0
-var crit_chance = 1
+var crit_chance = 0.95
 var slot_dragging = false
 var ad_clicked = false
 var slow_speed = 0.75
